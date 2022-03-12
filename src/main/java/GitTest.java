8f0ed974cf7e4d90f43826b605ce337e7d127209
xiaolanhe <xiaolanhe@163.com>
2022-03-12 19:00:15 +0800

    thrid commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -7,7 +7,5 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2!");
-        System.out.println("hello 3");
-        System.out.println("four");
     }
 }
