d87305a73c65b915121f1b495f07a0cc48f77447
xiaolanhe <xiaolanhe@163.com>
2022-03-12 18:56:33 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,11 @@
+
+/**
+ *@author: xiaolanhe
+ *@createDate: 2022/3/12 18:54
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+    }
+}
