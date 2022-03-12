ca51e8823a618c212a5b22fdbfd04328167ea962
xiaolanhe <xiaolanhe@163.com>
2022-03-12 19:18:26 +0800

    hot-fix commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("third");
         System.out.println("forth");
+        System.out.println("hot-fix add it");
     }
 }
